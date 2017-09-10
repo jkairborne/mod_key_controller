766382e1140d18a3c906a2038da2ac44980e5219
jk <jk@airsensing.com>
2017-09-10 17:57:59 -0400

    Experimental setup for exp2 on 20170910

--- src/position_controller.py
+++ src/position_controller.py
@@ -16,7 +16,7 @@ import serial, string, math, time, calendar
 
 # Import class that computes the desired positions
 from tf.transformations import euler_from_quaternion
-from geometry_msgs.msg import TransformStamped, Twist, TwistStamped, PoseStamped
+from geometry_msgs.msg import TransformStamped, Twist, TwistStamped, PoseStamped, Vector3
 
 
 class PositionController(object):
@@ -36,13 +36,18 @@ class PositionController(object):
 
 		self.opti_at_pbvs_receive = Twist()
 		self.PBVSOpti = True
+		self.CtrlToUse = "OPTI"
 
 		self.pbvs_data = Twist()
 		self.ibvs_data = Twist()
+		self.vis_deltas = Vector3()
+
+		self.targetInSight = False
 
 		# Pblishers and subscribers
 		self.sub_opti_data = rospy.Subscriber('/vrpn_client_node/{0}/pose'.format(self.model_name),PoseStamped, self.update_opti_data) # subscribe to incoming vicon data
 		self.pub_des_vel = rospy.Publisher('cmd_vel',Twist,queue_size=2) # publish desired velocity commands to quadrotor
+		self.visual_params = rospy.Publisher('visual_params',Vector3,queue_size=2) # publish desired velocity commands to quadrotor
 		self.pub_opti_data = rospy.Publisher('opti_data',PoseStamped,queue_size=2) # publish vicon data for use in RViz
 		self.sub_des_pose = rospy.Subscriber('des_pos',PoseStamped,self.des_pose) # subscribe to desired pose
 		self.sub_des_vel = rospy.Subscriber('des_vel',TwistStamped,self.des_velo) # subscribe to desired velocity
@@ -74,15 +79,18 @@ class PositionController(object):
 		self.prevTime = self.startTime; # previous loop time
 
 	def satur(self, val, satval):
-		if val>satval:
-			return satval
-		elif val<(-satval):
-			return (-satval)
-		else:
-			return val
+		return (min(satval,max(val,-satval)))
 
 	def des_pose(self, des_pos_msg): # subscriber to obtain desired pose
 		self.des_position = des_pos_msg
+		if (self.des_position.pose.position.x==-9999999) and self.des_position.pose.position.y==-9999999 and self.des_position.pose.position.z==9999999:
+			self.CtrlToUse="PBVS"
+		elif (self.des_position.pose.position.x==-9999998) and self.des_position.pose.position.y==-9999998 and self.des_position.pose.position.z==9999998:
+			self.CtrlToUse = "IBVS"
+		elif (self.des_position.pose.position.x==-9999997) and self.des_position.pose.position.y==-9999997 and self.des_position.pose.position.z==9999997:
+			self.CtrlToUse = "ZP-IBVS"
+		else:
+			self.CtrlToUse = "OPTI"
 
 	def des_velo(self, des_vel_msg): # subscriber to obtain desired velocity
 		self.des_velocity = des_vel_msg	
@@ -99,9 +107,24 @@ class PositionController(object):
 		#rospy.loginfo("x : %f    y : %f     z : %f      angular: %f   ", roll, pitch, z_vel, yaw_rate)
 
 	def update_PBVS_data(self, actual_pbvs_data): # subscriber to obtain vicon data
-		self.pbvs_data = actual_pbvs_data
-		self.opti_at_pbvs_receive.linear.x = self.opti_data.pose.position.x
-		self.opti_at_pbvs_receive.linear.y = self.opti_data.pose.position.y
+		self.pbvs_data = actual_pbvs_data 
+		self.deadReckoning = False # Just reset this to false every time.
+		if(self.PBVSOpti): #This means we are fusing the optitrack data with our visual data to create the vel cmd.
+			self.x_tgt = self.opti_data.pose.position.x - self.pbvs_data.angular.x # see September 9 2017 notes for why this is -ve
+			self.y_tgt = self.opti_data.pose.position.y + self.pbvs_data.angular.y
+			# Desired velocity
+			#self.x_vel_pbvs = 0
+			#self.y_vel_pbvs = 0
+			self.x_vel_pbvs = self.pbvs_data.linear.x
+			self.y_vel_pbvs = self.pbvs_data.linear.y
+			self.vis_deltas.x = self.x_tgt
+			self.vis_deltas.y = self.y_tgt
+			self.vis_deltas.z = 0
+			self.visual_params.publish(self.vis_deltas) # Publishes the optitrack coordinates we want to go to
+		if (self.pbvs_data.linear.x == 0 and self.pbvs_data.linear.y == 0 and self.pbvs_data.linear.z == 0 and self.pbvs_data.angular.x == 111 and self.pbvs_data.angular.y ==111 and self.pbvs_data.angular.z == 0):
+			self.targetInSight = False
+		else:
+			self.targetInSight = True
 		#rospy.loginfo("x : %f    y : %f     z : %f      angular: %f   ", roll, pitch, z_vel, yaw_rate)
 		
 	def update_IBVS_data(self, actual_ibvs_data):
@@ -129,7 +152,7 @@ class PositionController(object):
 		# Desired velocity
 		self.x_vel_des = self.des_velocity.twist.linear.x
 		self.y_vel_des = self.des_velocity.twist.linear.y
-
+		
 		# Actual position and orientation
 		self.x = self.opti_data.pose.position.x
 		self.y = self.opti_data.pose.position.y
@@ -147,8 +170,8 @@ class PositionController(object):
 
 		# Controller
 		f = (z_accel + self.g)/(cos(self.theta)*cos(self.phi))
-		temp_x_accel_comm = (4*self.zeta_x*self.omega_x*(self.x_vel_des-self.x_vel)) + 4*((self.omega_x**2)*(self.x_des-self.x))
-		temp_y_accel_comm = (4*self.zeta_y*self.omega_y*(self.y_vel_des-self.y_vel)) + 4*((self.omega_y**2)*(self.y_des-self.y))
+		temp_x_accel_comm = (3*self.zeta_x*self.omega_x*(self.x_vel_des-self.x_vel)) + 3*((self.omega_x**2)*(self.x_des-self.x))
+		temp_y_accel_comm = (3*self.zeta_y*self.omega_y*(self.y_vel_des-self.y_vel)) + 3*((self.omega_y**2)*(self.y_des-self.y))
 		
 		# Here we correct for varying psi angles:
 		x_accel_comm = temp_x_accel_comm*cos(self.psi) + temp_y_accel_comm * sin(self.psi)
@@ -162,51 +185,53 @@ class PositionController(object):
 		yaw_rate = (self.psi_des - self.psi)/self.tau_omega # yaw rate
 		#print("Modified: %.2f %.2f\n" % (pitch,-roll))
 
-		if (self.x_des==-9999999) and self.y_des==-9999999 and self.z_des==9999999:
+		if (self.CtrlToUse == "PBVS"):
 			if(self.PBVSOpti):
-			# In this case try to use the optitrack/PBVS controller.
-				self.x_des = self.opti_at_pbvs_receive.linear.x + self.pbvs_data.angular.x
-				self.y_des = self.opti_at_pbvs_receive.linear.y + self.pbvs_data.angular.y
-
-				# Desired velocity
-				self.x_vel_des = self.pbvs_data.linear.x
-				self.y_vel_des = self.pbvs_data.linear.y
+			# In this case try to use the optitrack/PBVS controller - using the x_vel_pbvs and x_tgt/y equivalents in the Pbvs callback.
+				#print("opti_x, pos_diff_x: %.2f %.2f" % (self.opti_at_pbvs_receive.linear.x ,self.pbvs_data.angular.x))
+				#print("opti_y, pos_diff_y: %.2f %.2f" % (self.opti_at_pbvs_receive.linear.y ,self.pbvs_data.angular.y))
+				print("x, y guessed by PBVS:  %.2f  %.2f" % (self.x_tgt, self.y_tgt))
 
 				# Controller
 				f = (z_accel + self.g)/(cos(self.theta)*cos(self.phi))
-				temp_x_accel_comm = (self.zeta_x*self.omega_x*(self.x_vel_des-self.x_vel)) + ((self.omega_x**2)*(self.x_des-self.x))
-				temp_y_accel_comm = (self.zeta_y*self.omega_y*(self.y_vel_des-self.y_vel)) + ((self.omega_y**2)*(self.y_des-self.y))
+				temp_x_accel_comm = (self.zeta_x*self.omega_x*(self.x_vel_pbvs-self.x_vel)) + ((self.omega_x**2)*(self.x_tgt-self.x))
+				temp_y_accel_comm = (self.zeta_y*self.omega_y*(self.y_vel_pbvs-self.y_vel)) + ((self.omega_y**2)*(self.y_tgt-self.y))
 		
 				# Here we correct for varying psi angles:
 				x_accel_comm = temp_x_accel_comm*cos(self.psi) + temp_y_accel_comm * sin(self.psi)
 				y_accel_comm = temp_x_accel_comm*sin(self.psi) - temp_y_accel_comm * cos(self.psi)
 
-				print("x_des, opti_rx_x, pbvs_x, x_vel_des: %.2f  %.2f  %.2f  %.2f" % (self.x_des, self.opti_at_pbvs_receive.linear.x,self.pbvs_data.angular.x,self.x_vel_des))
-				print("y_des, opti_rx_y, pbvs_y, y_vel_des: %.2f  %.2f  %.2f  %.2f" % (self.y_des, self.opti_at_pbvs_receive.linear.y,self.pbvs_data.angular.y,self.y_vel_des))
-
-				roll = asin(min(1,max((-y_accel_comm/f),-1))) # roll angle
+				roll = asin(min(1,max((y_accel_comm/f),-1))) # roll angle
 				pitch = asin(min(1,max((x_accel_comm/(f*cos(roll))),-1))) #pitch angle
 
 				self.quadrotor_command.linear.x = self.satur(pitch,self.stval)
-				self.quadrotor_command.linear.y = self.satur(-roll,self.stval)
+				self.quadrotor_command.linear.y = self.satur(roll,self.stval)
 				self.quadrotor_command.linear.z = self.pbvs_data.linear.z
 				self.quadrotor_command.angular.x = 0
 				self.quadrotor_command.angular.y = 0
 				self.quadrotor_command.angular.z = self.pbvs_data.angular.z
-				print("x and y commanded: %.2f %.2f" % (self.quadrotor_command.linear.x,self.quadrotor_command.linear.y))
 				#print("PBVS control")
 				#print("PBVS control: %.2f %.2f %.2f %.2f" % (self.quadrotor_command.linear.x,self.quadrotor_command.linear.y,self.quadrotor_command.linear.z,self.quadrotor_command.angular.z))
-			else:
+
+				if (self.targetInSight == False): # This means that the target is no longer in the FOV
+					self.quadrotor_command.linear.x = 0
+					self.quadrotor_command.linear.y = 0
+					self.quadrotor_command.linear.z = 0
+					self.quadrotor_command.angular.x = 0.1 #enable autohover
+					self.quadrotor_command.angular.y = 0
+					self.quadrotor_command.angular.z = 0
+
+			else: #PBVS without Opti
 				self.quadrotor_command.linear.x = self.satur(self.pbvs_data.linear.x,self.stval)
 				self.quadrotor_command.linear.y = self.satur(self.pbvs_data.linear.y,self.stval)
 				self.quadrotor_command.linear.z = self.pbvs_data.linear.z
 				self.quadrotor_command.angular.x = 0
 				self.quadrotor_command.angular.y = 0
 				self.quadrotor_command.angular.z = self.pbvs_data.angular.z
-		elif (self.x_des==-9999998) and self.y_des==-9999998 and self.z_des==9999998:
+		elif self.CtrlToUse=="IBVS":
 			self.quadrotor_command = self.ibvs_data
 			#print("IBVS control")
-		elif (self.x_des==-9999997) and self.y_des==-9999997 and self.z_des==9999997:
+		elif self.CtrlToUse =="ZP-IBVS":
 			self.quadrotor_command.linear.x = pitch #command from optitrack
 			self.quadrotor_command.linear.y = -roll #command from optitrack
 			self.quadrotor_command.linear.z = self.ibvs_data.linear.z
@@ -218,17 +243,9 @@ class PositionController(object):
 			self.quadrotor_command.linear.y = -roll
 			self.quadrotor_command.angular.z = -yaw_rate
 			self.quadrotor_command.linear.z = -z_vel
-			#print("opti control: %.2f %.2f %.2f %.2f" % (self.quadrotor_command.linear.x,self.quadrotor_command.linear.y,self.quadrotor_command.linear.z,self.quadrotor_command.angular.z))
+		#print("pos_controller cmd_sent: %.2f %.2f %.2f %.2f" % (self.quadrotor_command.linear.x,self.quadrotor_command.linear.y,self.quadrotor_command.linear.z,self.quadrotor_command.angular.z))
 		self.pub_des_vel.publish(self.quadrotor_command)
 		#rospy.loginfo("x : %f    y : %f     z : %f      angular: %f   ", roll, pitch, z_vel, yaw_rate)
-
-#		print("\nx,y,z: %.2f \t %.2f \t %.2f" %(self.x,self.y,self.z))
-#		print("desx,y,z: %.2f \t %.2f \t %.2f" %(self.x_des,self.y_des,self.z_des))
-#		print("roll: %.2f, pitch:  %.2f, yaw:  %.2f" %(self.phi,self.theta,self.psi) )
-#		print("Delta xdot,ydot,z,psi: %.2f \t %.2f \t %.2f \t %.2f" % ((self.x_vel_des-self.x_vel),(self.y_vel_des-self.y_vel),(self.z_des - self.z),(self.psi_des - self.psi)))
-#		print("Delta x,y: %.2f \t %.2f" % ((self.x_des-self.x),(self.y_des-self.y)))
-#		print("commanded x : %.2f    y : %.2f     z : %.2f      angular: %.2f   "%(self.quadrotor_command.linear.x, self.quadrotor_command.linear.y, self.quadrotor_command.linear.z, self.quadrotor_command.angular.z))
-		#print("roll: %.2f, pitch:  %.2f, yaw:  %.2f" %(self.phi,self.theta,self.psi) )
 
 		# Publish vicon data as PoseStamped geometry msg
 		self.pub_opti_data.publish(self.opti_pose)
