f344262b54da981eea2046a48835588c5baa82d1
jk <jk@airsensing.com>
2017-09-14 10:42:58 -0400

    much simplified version of position controller, now only meant for the use of optitrack data, with the option of adding roomba data if required

--- src/position_controller.py
+++ src/position_controller.py
@@ -34,17 +34,22 @@ class PositionController(object):
 		self.quadrotor_command = Twist() # container to publish command velocity to quadrotor
 
 		self.opti_at_pbvs_receive = Twist()
-		self.PBVSOpti = True
+		
+		self.addRmba = False
+		self.x_tgt = 0
+		self.y_tgt = 0
+		self.x_vel_pbvs = 0
+		self.y_vel_pbvs = 0
+
 		self.CtrlToUse = "OPTI"
-		self.useZScale = True
-		self.scaleZHeight = -0.5
+		self.useZScale = False
+		self.scaleZHeight = -0.5 #remember Z is negative upwards
 		self.ZScale = 0.2
 
 		self.pbvs_data = Twist()
 		self.ibvs_data = Twist()
 		self.vis_deltas = Twist()
 
-		self.targetInSight = False
 		self.deadReckoning = False
 		self.DR_posx =0
 		self.DR_posy =0 
@@ -113,39 +118,28 @@ class PositionController(object):
 	def update_PBVS_data(self, actual_pbvs_data): # subscriber to obtain pbvs data
 		self.pbvs_data = actual_pbvs_data 
 		self.deadReckoning = False
-		if(self.pbvs_data.linear.x==-1000): #This if condition is likely useless
-			self.targetInSight = False
-			 #target has been out of FOV for long enough that we give up hope.
-		elif(self.pbvs_data.linear.x==-500): #get it back to normal 
-			self.targetInSight = False
+		if(self.pbvs_data.angular.x==-2000):
 			self.deadReckoning = True
-			#print("self.deadReckoning = %r" % self.deadReckoning) # no need to do anything else - this simply won't update x_tgt and y_tgt below
-		elif(8<self.pbvs_data.linear.x <12):
-			self.pbvs_data.linear.x -= 10 #get it back to normal
-			self.targetInSight = True
-		elif(-2<self.pbvs_data.linear.x<2):
-			self.targetInSight = True
-			
-		if(self.PBVSOpti): #This means we are fusing the optitrack data with our visual data to create the vel cmd.
-			# Desired velocity
-			self.x_vel_pbvs = 0
-			self.y_vel_pbvs = 0
-			#self.x_vel_pbvs = self.pbvs_data.linear.x
-			#self.y_vel_pbvs = self.pbvs_data.linear.y
-			if(self.deadReckoning == False):
-				self.x_tgt = self.opti_data.pose.position.x + (-self.pbvs_data.angular.x*cos(self.psi))#-self.pbvs_data.angular.y*sin(self.psi)) # see September 9 2017 notes for why this is -ve
-				self.y_tgt = self.opti_data.pose.position.y + (self.pbvs_data.angular.y*cos(self.psi))#-self.pbvs_data.angular.x*sin(self.psi))
-				#print("opti x: %.2f opti y: %.2f opti yaw: %.2f" % (self.opti_data.pose.position.x,self.opti_data.pose.position.y,self.psi) )
-			else: #we are DR
-				pass#print("Dead-reckoning")
+		# Desired velocity
+		self.x_vel_pbvs = 0
+		self.y_vel_pbvs = 0
+		#self.x_vel_pbvs = self.pbvs_data.linear.x
+		#self.y_vel_pbvs = self.pbvs_data.linear.y
+		if(self.deadReckoning == False):
+			self.x_tgt = self.opti_data.pose.position.x + (-self.pbvs_data.angular.x*cos(self.psi)-self.pbvs_data.angular.y*sin(self.psi)) # see September 9 2017 notes for why this is -ve
+			self.y_tgt = self.opti_data.pose.position.y + (self.pbvs_data.angular.y*cos(self.psi)-self.pbvs_data.angular.x*sin(self.psi))
+			#print("opti x: %.2f opti y: %.2f opti yaw: %.2f" % (self.opti_data.pose.position.x,self.opti_data.pose.position.y,self.psi) )
+		else: #we are DR
+			pass#print("Dead-reckoning")
 
-			self.vis_deltas.linear.x = self.x_tgt #for publishing to topic
-			self.vis_deltas.linear.y = self.y_tgt
-			self.vis_deltas.linear.z = self.opti_data.pose.position.x
-			self.vis_deltas.angular.x = self.opti_data.pose.position.y
-			self.vis_deltas.angular.y = self.pbvs_data.angular.x
-			self.vis_deltas.angular.z = self.psi
-			self.visual_params.publish(self.vis_deltas) # Publishes the optitrack coordinates we want to go to - 
+		self.vis_deltas.linear.x = self.x_tgt #for publishing to topic
+		self.vis_deltas.linear.y = self.y_tgt
+		self.vis_deltas.linear.z = self.opti_data.pose.position.x
+		self.vis_deltas.angular.x = self.opti_data.pose.position.y
+		self.vis_deltas.angular.y = self.pbvs_data.angular.x
+		self.vis_deltas.angular.z = self.psi
+		self.visual_params.publish(self.vis_deltas) # Publishes the optitrack coordinates we want to go to - 
+		#print("just published visual_deltas")
 
 		#rospy.loginfo("x : %f    y : %f     z : %f      angular: %f   ", roll, pitch, z_vel, yaw_rate)
 		
@@ -184,71 +178,39 @@ class PositionController(object):
 		self.z_vel = (self.z - self.z_prev)/dt
 		z_accel = (self.z_vel - self.z_vel_prev)/dt
 
-		self.x_vel_des = 0
-		self.y_vel_des = 0
-		# Controller
-		f = (z_accel + self.g)/(cos(self.theta)*cos(self.phi))
-		temp_x_accel_comm = 3*(self.zeta_x*self.omega_x*(self.x_vel_des-self.x_vel)) + 3*((self.omega_x**2)*(self.x_des-self.x))
-		temp_y_accel_comm = 3*(self.zeta_y*self.omega_y*(self.y_vel_des-self.y_vel)) + 3*((self.omega_y**2)*(self.y_des-self.y))
-		
-		# Here we correct for varying psi angles:
-		x_accel_comm = temp_x_accel_comm*cos(self.psi) + temp_y_accel_comm * sin(self.psi)
-		y_accel_comm = temp_x_accel_comm*sin(self.psi) - temp_y_accel_comm * cos(self.psi)
 
-		
-
-		roll = asin(min(1,max((-y_accel_comm/f),-1))) # roll angle
-		pitch = asin(min(1,max((x_accel_comm/(f*cos(roll))),-1))) #pitch angle
-		z_vel = (self.z_des - self.z)/self.tau_z # z velocity
-		yaw_rate = (self.psi_des - self.psi)/self.tau_omega # yaw rate
 		#print("Modified: %.2f %.2f\n" % (pitch,-roll))
 
 		if (self.CtrlToUse == "PBVS"):
-			if(self.PBVSOpti):
-				if(self.targetInSight or self.deadReckoning):
-				# In this case try to use the optitrack/PBVS controller - using the x_vel_pbvs and x_tgt/y equivalents in the Pbvs callback.
-					#print("x, y guessed by PBVS:  %.2f  %.2f" % (self.x_tgt, self.y_tgt))
+		# In this case try to use the optitrack/PBVS controller - using the x_vel_pbvs and x_tgt/y equivalents in the Pbvs callback.
+			#print("x, y guessed by PBVS:  %.2f  %.2f" % (self.x_tgt, self.y_tgt))
 
-					# Controller
-					f = (z_accel + self.g)/(cos(self.theta)*cos(self.phi))
-					#print("vel and pos components: %.2f %.2f" % (3*(self.zeta_x*self.omega_x*(self.x_vel_pbvs-self.x_vel)),((self.omega_x**2)*(self.x_tgt-self.x))))
-					temp_x_accel_comm = 3*(self.zeta_x*self.omega_x*(self.x_vel_pbvs-self.x_vel)) + (self.omega_x**2)*(self.x_tgt-self.x)
-					temp_y_accel_comm = 3*(self.zeta_y*self.omega_y*(self.y_vel_pbvs-self.y_vel)) + (self.omega_y**2)*(self.y_tgt-self.y)
-		
-					# Here we correct for varying psi angles:
-					x_accel_comm = temp_x_accel_comm*cos(self.psi) + temp_y_accel_comm * sin(self.psi)
-					y_accel_comm = temp_x_accel_comm*sin(self.psi) - temp_y_accel_comm * cos(self.psi)
+			# Controller
+			f = (z_accel + self.g)/(cos(self.theta)*cos(self.phi))
+			#print("vel and pos components: %.2f %.2f" % (3*(self.zeta_x*self.omega_x*(self.x_vel_pbvs-self.x_vel)),((self.omega_x**2)*(self.x_tgt-self.x))))
+			temp_x_accel_comm = 3*(self.zeta_x*self.omega_x*(self.x_vel_pbvs-self.x_vel)) + (self.omega_x**2)*(self.x_tgt-self.x)
+			temp_y_accel_comm = 3*(self.zeta_y*self.omega_y*(self.y_vel_pbvs-self.y_vel)) + (self.omega_y**2)*(self.y_tgt-self.y)
 
-					roll = asin(min(1,max((y_accel_comm/f),-1))) # roll angle
-					pitch = asin(min(1,max((x_accel_comm/(f*cos(roll))),-1))) #pitch angle
+			# Here we correct for varying psi angles:
+			x_accel_comm = temp_x_accel_comm*cos(self.psi) + temp_y_accel_comm * sin(self.psi)
+			y_accel_comm = temp_x_accel_comm*sin(self.psi) - temp_y_accel_comm * cos(self.psi)
 
-					self.quadrotor_command.linear.x = self.satur(pitch,self.stval)
-					self.quadrotor_command.linear.y = self.satur(roll,self.stval)
-					self.quadrotor_command.linear.z = self.pbvs_data.linear.z
-					self.quadrotor_command.angular.x = 0
-					self.quadrotor_command.angular.y = 0
-					self.quadrotor_command.angular.z = self.pbvs_data.angular.z #if DR, this should come in as 0.1
-					#print("TinSight or deadReckoning: values: %.2f %.2f" % (self.quadrotor_command.linear.x,self.quadrotor_command.linear.y))
- 
-					#print("PBVS control")
-					#print("PBVS control: %.2f %.2f %.2f %.2f" % (self.quadrotor_command.linear.x,self.quadrotor_command.linear.y,self.quadrotor_command.linear.z,self.quadrotor_command.angular.z))
+			roll = asin(min(1,max((y_accel_comm/f),-1))) # roll angle
+			pitch = asin(min(1,max((x_accel_comm/(f*cos(roll))),-1))) #pitch angle
 
-				if (self.targetInSight == False and self.deadReckoning == False): # This means that the target is no longer in the FOV
-					self.quadrotor_command.linear.x = 0
-					self.quadrotor_command.linear.y = 0
-					self.quadrotor_command.linear.z = 0
-					self.quadrotor_command.angular.x = 0.1 #enable autohover
-					self.quadrotor_command.angular.y = 0
-					self.quadrotor_command.angular.z = 0
-					#print("TnotinSight, not DR")
+			self.quadrotor_command.linear.x = self.satur(pitch,self.stval)
+			self.quadrotor_command.linear.y = self.satur(roll,self.stval)
+			self.quadrotor_command.linear.z = self.pbvs_data.linear.z
+			self.quadrotor_command.angular.x = 0
+			self.quadrotor_command.angular.y = 0
+			self.quadrotor_command.angular.z = self.pbvs_data.angular.z #if DR, this should come in as 0.1
+			#print("now in PBVS")
 
-			else: #PBVS without Opti
-				self.quadrotor_command.linear.x = self.satur(self.pbvs_data.linear.x,self.stval)
-				self.quadrotor_command.linear.y = self.satur(self.pbvs_data.linear.y,self.stval)
-				self.quadrotor_command.linear.z = self.pbvs_data.linear.z
-				self.quadrotor_command.angular.x = 0
-				self.quadrotor_command.angular.y = 0
-				self.quadrotor_command.angular.z = self.pbvs_data.angular.z
+			if(self.addRmba):
+				self.quadrotor_command.linear.x += self.pbvs_data.linear.x
+				self.quadrotor_command.linear.y += self.pbvs_data.linear.y
+				#print("Now adding Roooooomba")
+			
 		elif self.CtrlToUse=="IBVS":
 			self.quadrotor_command = self.ibvs_data
 			#print("IBVS control")
@@ -259,6 +221,22 @@ class PositionController(object):
 			self.quadrotor_command.angular.z = self.ibvs_data.angular.z
 			#print("ZP_IBVS control")
 		else:
+			self.x_vel_des = 0
+			self.y_vel_des = 0
+			# Controller
+			f = (z_accel + self.g)/(cos(self.theta)*cos(self.phi))
+			temp_x_accel_comm = 3*(self.zeta_x*self.omega_x*(self.x_vel_des-self.x_vel)) + 3*((self.omega_x**2)*(self.x_des-self.x))
+			temp_y_accel_comm = 3*(self.zeta_y*self.omega_y*(self.y_vel_des-self.y_vel)) + 3*((self.omega_y**2)*(self.y_des-self.y))
+	
+			# Here we correct for varying psi angles:
+			x_accel_comm = temp_x_accel_comm*cos(self.psi) + temp_y_accel_comm * sin(self.psi)
+			y_accel_comm = temp_x_accel_comm*sin(self.psi) - temp_y_accel_comm * cos(self.psi)
+
+			roll = asin(min(1,max((-y_accel_comm/f),-1))) # roll angle
+			pitch = asin(min(1,max((x_accel_comm/(f*cos(roll))),-1))) #pitch angle
+			z_vel = (self.z_des - self.z)/self.tau_z # z velocity
+			yaw_rate = (self.psi_des - self.psi)/self.tau_omega # yaw rate
+
 			# Just use the original optitrack control from above
 			self.quadrotor_command.linear.x = pitch
 			self.quadrotor_command.linear.y = -roll
@@ -266,8 +244,6 @@ class PositionController(object):
 			self.quadrotor_command.linear.z = -z_vel
 			#print("optitrack control")
 		#print("pos_controller cmd_sent: %.2f %.2f %.2f %.2f" % (self.quadrotor_command.linear.x,self.quadrotor_command.linear.y,self.quadrotor_command.linear.z,self.quadrotor_command.angular.z))
-
-#		self.quadrotor_command.linear.x = 1.0
 		
 		#Scale command to height of quad if below 0.5m
 		if self.useZScale and (self.scaleZHeight<self.z<0.1): #ensure we want to use ZScale. Also keep in mind NED frame, so z is negative at altitude. The 0.1 is just in case of weird calibration
@@ -286,7 +262,6 @@ class PositionController(object):
 		self.y_prev = self.y
 		self.z_prev = self.z
 		self.z_vel_prev = self.z_vel
-		#print("ctrlToUse: %r PBVSOpti: %r deadReckoning: %r, targetInSight: %r " % (self.CtrlToUse,self.PBVSOpti,self.deadReckoning,self.targetInSight))
 
 if __name__ == '__main__':
 	rospy.init_node('position_controller') # initialize node
