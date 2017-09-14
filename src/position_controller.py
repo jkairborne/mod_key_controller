992292351454fd1b8a79cd88c87556ae4a4e54f6
jk <jk@airsensing.com>
2017-09-14 15:27:24 -0400

    This is the setup used in exp4 20170914 - calibrating the roomba velocity for the ardrone

--- src/position_controller.py
+++ src/position_controller.py
@@ -35,7 +35,7 @@ class PositionController(object):
 
 		self.opti_at_pbvs_receive = Twist()
 		
-		self.addRmba = False
+		self.addRmba = True
 		self.x_tgt = 0
 		self.y_tgt = 0
 		self.x_vel_pbvs = 0
@@ -180,11 +180,14 @@ class PositionController(object):
 
 
 		#print("Modified: %.2f %.2f\n" % (pitch,-roll))
+		self.quadrotor_command.linear.x = 0
+		self.quadrotor_command.linear.y = 0
+
 
 		if (self.CtrlToUse == "PBVS"):
 		# In this case try to use the optitrack/PBVS controller - using the x_vel_pbvs and x_tgt/y equivalents in the Pbvs callback.
 			#print("x, y guessed by PBVS:  %.2f  %.2f" % (self.x_tgt, self.y_tgt))
-
+			'''
 			# Controller
 			f = (z_accel + self.g)/(cos(self.theta)*cos(self.phi))
 			#print("vel and pos components: %.2f %.2f" % (3*(self.zeta_x*self.omega_x*(self.x_vel_pbvs-self.x_vel)),((self.omega_x**2)*(self.x_tgt-self.x))))
@@ -197,15 +200,48 @@ class PositionController(object):
 
 			roll = asin(min(1,max((y_accel_comm/f),-1))) # roll angle
 			pitch = asin(min(1,max((x_accel_comm/(f*cos(roll))),-1))) #pitch angle
+			self.x_vel_des = 0
+			self.y_vel_des = 0
+			'''
 
-			self.quadrotor_command.linear.x = self.satur(pitch,self.stval)
-			self.quadrotor_command.linear.y = self.satur(roll,self.stval)
-			self.quadrotor_command.linear.z = self.pbvs_data.linear.z
-			self.quadrotor_command.angular.x = 0
-			self.quadrotor_command.angular.y = 0
-			self.quadrotor_command.angular.z = self.pbvs_data.angular.z #if DR, this should come in as 0.1
+
+
+			self.x_vel_des = 0
+			self.y_vel_des = 0
+			self.x_des = 0.4
+			self.y_des = 0.2
+			self.z_des = -1.0
+			# Controller
+			f = (z_accel + self.g)/(cos(self.theta)*cos(self.phi))
+			temp_x_accel_comm = 3*(self.zeta_x*self.omega_x*(self.x_vel_des-self.x_vel)) + 3*((self.omega_x**2)*(self.x_des-self.x))
+			temp_y_accel_comm = 3*(self.zeta_y*self.omega_y*(self.y_vel_des-self.y_vel)) + 3*((self.omega_y**2)*(self.y_des-self.y))
+	
+			# Here we correct for varying psi angles:
+			x_accel_comm = temp_x_accel_comm*cos(self.psi) + temp_y_accel_comm * sin(self.psi)
+			y_accel_comm = temp_x_accel_comm*sin(self.psi) - temp_y_accel_comm * cos(self.psi)
+
+			roll = asin(min(1,max((-y_accel_comm/f),-1))) # roll angle
+			pitch = asin(min(1,max((x_accel_comm/(f*cos(roll))),-1))) #pitch angle
+			z_vel = (self.z_des - self.z)/self.tau_z # z velocity
+			yaw_rate = (self.psi_des - self.psi)/self.tau_omega # yaw rate
+
+			# Just use the original optitrack control from above
+			self.quadrotor_command.linear.x = pitch
+			#self.quadrotor_command.linear.y = -roll
+			self.quadrotor_command.angular.z = -yaw_rate
+			self.quadrotor_command.linear.z = -z_vel
+			#self.quadrotor_command.linear.x = self.satur(pitch,self.stval)
+			#self.quadrotor_command.linear.y = self.satur(roll,self.stval)
+
+
+
+
+			#self.quadrotor_command.linear.z = self.pbvs_data.linear.z
+			#self.quadrotor_command.angular.x = 0
+			#self.quadrotor_command.angular.y = 0
+			#self.quadrotor_command.angular.z = self.pbvs_data.angular.z #if DR, this should come in as 0.1
 			#print("now in PBVS")
-
+			
 			if(self.addRmba):
 				self.quadrotor_command.linear.x += self.pbvs_data.linear.x
 				self.quadrotor_command.linear.y += self.pbvs_data.linear.y
